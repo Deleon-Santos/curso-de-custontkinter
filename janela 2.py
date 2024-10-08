ad9da5bdf25d02cff63727e5a648a1ff443a15c4
Deleon <delps.santos1987@gmail.com>
2024-10-08 19:41:22 -0300

    aulas 5 de custontkinter

--- janela 2.py
+++ janela 2.py
@@ -14,7 +14,7 @@ janela.resizable(width=True, height=True) # janela pode ser flexivel ou nao
 
 #crindo um frame no custonTkinter
 frame1 = ctk.CTkFrame(master=janela,width=200,height=330, fg_color=('white'), bg_color='blue' ,corner_radius=10).place(x=10, y=60) # estou definindo a posição e tamanho do frame
-frame2 = ctk.CTkFrame(janela, width=200 , height=330, fg_color='teal' ).place(x=230, y=60)
+frame2 = ctk.CTkFrame(janela, width=200 , height=330, fg_color='teal' ).place(x=230, y=60) # aqui tambem podemos atribuir bordas e cores de fundo aos frames
 frame2 = ctk.CTkFrame(janela, width=200 , height=330,fg_color='red' ).place(x=450, y=60) 
 
 btn = ctk.CTkButton(janela, text="ok")  # O botão recebe o comando CTkButton (corrigido) e integra a janela com o texto
@@ -23,11 +23,24 @@ btn.pack()# criando uma botão no custontkinter
 #costumizando o tema da nossa aplicação
 janela._set_appearance_mode("light")#comando para trocar o tema da aplicação
 
-def nova_tela():
+tabview=ctk.CTkTabview(janela,width=500 corne_radius=20, border_width=1,border_color='red',
+fg_color='teal', segmented_button_fg_color='red', segmented_button_selected_color= 'green,')
+
+tabview.pack()
+tabview.add('idade')
+tabview.add('nome')
+tabview.add('genero')
+tabview.tab('idade').grid_columnconfigure(0,weight=1)
+tabview.tab('idade').grid_columnconfigure(0,weight=1)
+tabview.tab('idade').grid_columnconfigure(0,weight=1)
+
+
+
+"""def nova_tela():
     nova_janela = ctk.CTkToplevel(janela, fg_color='light')# a nova janela vais receber o toplevel da antiga janela
     nova_janela.geometry('700x400')
 btn.nova_tela = ctk.CTkButton(master=janela, text='Nova Janela',command=nova_tela).place(x=40, y=90)#aqui foi crido uma botão para invocarmos uma nova janela
-    
+    """
 janela.mainloop()  # Loop para manter a janela aberta (corrigido)
 
 
